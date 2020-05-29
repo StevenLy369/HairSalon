59699276869d9ff0eaaec9168e58453e41e20310
Steven Ly <1stevenly92@gmail.com>
2020-05-29 08:44:39 -0700

    finished models i think

---
+++ HairSalon.Solutions/Models/Client.cs
@@ -0,0 +1,17 @@
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System;
+
+namespace HairSalon.Models
+{
+  public class Client
+  {
+    public int ClientId { get; set; }
+    public string Name { get; set; }
+    public virtual ICollection<Stylist> Stylists { get; set; }
+    public Client()
+    {
+      this.Stylists = new HashSet<Stylist>();
+    }
+  }
+}
